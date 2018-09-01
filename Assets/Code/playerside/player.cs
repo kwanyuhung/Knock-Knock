601152e02ed79410c969194341b0210ab38c2c29
kwanyuhung <kwanyuhung@gmail.com>
2018-09-01 16:44:35 +0800

    new
    update

---
+++ Assets/Code/playerside/player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class player{
+
+
+
+	public player(string UserName, int LV, float Exp, int stone , int money){
+
+
+	}
+
+
+}
