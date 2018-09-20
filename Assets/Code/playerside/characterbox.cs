8a7af7dce8922f6122e1027bc2c6c0425a3284ba
kwanyuhung <kwanyuhung@gmail.com>
2018-09-21 01:41:13 +0800

    l

---
+++ Assets/Code/playerside/characterbox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterbox : MonoBehaviour {
+
+	
+
+    public characterbox(List<Character> Character)
+    {
+
+
+    }
+}
--- Assets/Code/playerside/player.cs
+++ Assets/Code/playerside/player.cs
@@ -6,10 +6,19 @@ public class player{
 
 
 
-	public player(string UserName, int LV, float Exp, int stone , int money){
 
 
-	}
+	public player(string UserName, int LV, float Exp, int Stone , int Money,int MovePoint, characterbox C){
+        UserName = "noting";
+        LV = 1;
+        Exp = 0;
+        Stone = 0;
+        Money = 0;
+        MovePoint = 100;
+
+    }
 
 
+
+    
 }
